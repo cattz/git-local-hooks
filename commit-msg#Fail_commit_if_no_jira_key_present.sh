a628b48cbb1c2a868a1e628db40f6bc7e332885a
Xabier Davila <davila.xabier@gmail.com>
2020-03-10 09:12:01 +0100

    Add #noissue to regex

--- commit-msg#Fail_commit_if_no_jira_key_present.sh
+++ commit-msg#Fail_commit_if_no_jira_key_present.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Rejects commit messages not starting with a Jira key [XXX-999] (Square brackes are optional)
 
-[[ $(head $1) =~ ^\[?[A-Z][A-Z_0-9]+-[0-9]+.* ]] && exit 0
+[[ $(head $1) =~ ^(\[?([A-Z][A-Z_0-9]+-[0-9]+)|(\#noissue))(.|\n)*(?s) ]] && exit 0
 
 cat << EOF
 ╔═╗┌─┐┌┬┐┌┬┐┬┌┬┐  ╦═╗┌─┐ ┬┌─┐┌─┐┌┬┐┌─┐┌┬┐
